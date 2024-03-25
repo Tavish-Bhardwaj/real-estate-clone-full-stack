5df9dba1f54267fc2b36bdc7b8dfa24dde40fe87
Tavish Bhardwaj <bhardwajtavishofficial@gmail.com>
2024-03-25 16:27:37 +0530

    Companies section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-
+import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
+import Companies from "./Components/Companies/Companies";
+import './App.css';
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+<div className="white-gradient"/>
+    <Header/>
+    <Hero/>
+
+      </div>
+      <Companies/>
+    </div>
   );
 }
 
