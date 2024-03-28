7047aef51ba75a357db80f97609b69ebd43c9f25
Tavish Bhardwaj <bhardwajtavishofficial@gmail.com>
2024-03-28 19:55:08 +0530

    static version completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
 import Companies from "./Components/Companies/Companies";
-import './App.css';
+import "./App.css";
 import Residencies from "./Components/Residencies/Residencies";
 import Value from "./Components/Values/Values";
+import Contact from "./Components/Contact/Contact";
+import GetStarted from "./Components/GetStarted/GetStarted";
+import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div className="App">
       <div>
-<div className="white-gradient"/>
-    <Header/>
-    <Hero/>
-
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
       </div>
-      <Companies/>
-      <Residencies/>
-      <Value/>
+      <Companies />
+      <Residencies />
+      <Value />
+      <Contact />
+      <GetStarted />
+      <Footer />
     </div>
   );
 }
